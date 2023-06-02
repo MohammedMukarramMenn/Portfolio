10424c3ea42bec44a3c50ccc8f1e6fb9ffe5ed2b
MohammedMukarramMenn <mukarrammenn@gmail.com>
2023-06-02 16:27:53 +0530

    link updation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,17 +19,17 @@ const Header = () => (
   <Container>
     <Div1>
       <li>
-        <Link href="#projects" legacyBehavior>
+        <Link href="https://uatportfolio.vercel.app/#projects" legacyBehavior>
           <NavLink>Projects</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#skills" legacyBehavior>
+        <Link href="https://uatportfolio.vercel.app/#skills" legacyBehavior>
           <NavLink>Skills</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#about" legacyBehavior>
+        <Link href="https://uatportfolio.vercel.app/#about" legacyBehavior>
           <NavLink>About</NavLink>
         </Link>
       </li>
