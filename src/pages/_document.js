b01f6785d8c5689fc1d92af358dc005fbeba43f9
MohammedMukarramMenn <mukarrammenn@gmail.com>
2023-07-25 11:20:40 +0530

    SEO Changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,6 +35,7 @@ export default class MyDocument extends Document {
           <meta name="title" content="Mohmmed Mukarram Menn - Full Stack Developer" />
           <meta name="description" content="Meet Mohmmed Mukarram Menn, a professional full stack web developer seeking freelance projects." />
           <meta property="og:type" content="website" />
+<<<<<<< Updated upstream
           <meta property="og:title" content="Mohmmed Mukarram Menn - Full Stack Developer" />
           <meta property="og:description" content="With a strong command of front-end, back-end & server technologies, [Your Name] delivers seamless web solutions with a focus on quality and client satisfaction. " />
           <meta property="og:image" content="https://www.vipuljha.com/images/banner.png" />
@@ -42,6 +43,39 @@ export default class MyDocument extends Document {
           <meta property="twitter:title" content="Mohmmed Mukarram Menn - Full Stack Developer" />
           <meta property="twitter:description" content="Meet Mohmmed Mukarram Menn, a professional full stack web developer seeking freelance projects." />
           <meta property="twitter:image" content="https://www.vipuljha.com/images/banner.png" />
+=======
+          <meta
+            property="og:title"
+            content="Mohmmed Mukarram Menn - Full Stack Developer"
+          />
+          <meta
+            property="og:description"
+            content="With a strong command of front-end, back-end & server technologies, [Your Name] delivers seamless web solutions with a focus on quality and client satisfaction. "
+          />
+          {/* <meta
+            property="og:image"
+            content="https://www.vipuljha.com/images/banner.png"
+          /> */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:title"
+            content="Mohmmed Mukarram Menn - Full Stack Developer"
+          />
+          <meta
+            property="twitter:description"
+            content=" I'm Mohmmed Mukarram Menn, and I'm a full stack web developer. I'm passionate about crafting awesome applications and designs for the web. If you have a cool idea in mind, let's team up and bring it to life! Let's make something amazing together!"
+          />
+          {/* <meta
+            property="twitter:image"
+            content="https://www.vipuljha.com/images/banner.png"
+          /> */}
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+            crossOrigin="anonymous"
+          />
+>>>>>>> Stashed changes
         </Head>
         <body>
           <Main />
